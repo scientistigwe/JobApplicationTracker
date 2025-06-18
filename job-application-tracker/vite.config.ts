fa1c509cea96f2d3c45d59687c1dad6786ea0060
Chibueze Igwe <scientistigwe@gmail.com>
2025-06-18 12:53:40 +0100

    Updates

--- job-application-tracker/vite.config.ts
+++
@@ -1,8 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/JobApplicationTracker/', // Must match your repo name
-})
